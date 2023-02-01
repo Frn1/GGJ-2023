0dcec7b0ec7b4a5b224d8bd7b1bbb69fb7a751ea
frn1 <20214662+frn1@users.noreply.github.com>
2023-02-01 17:12:45 -0300

    Reworked how velocity was calculated

--- Assets/PlayerMovementController.cs
+++ Assets/PlayerMovementController.cs
@@ -8,41 +8,32 @@ public class PlayerMovementController : MonoBehaviour
 {
     public float maxSpeed = 15f;
 
-    public float speed = 15f;
-    public float stoppingForce = 1.5f;
+    public float accelTime = 0.01f;
+    public float stopTime = 0.01f;
 
     private Rigidbody2D rigidbody2D;
+
+    private float runningTime = 0f;
+    private float stoppedTime = 0f;
 
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
 
-        Vector2 movement = new Vector2(horizontal, 0);
+        Vector2 velocity = new Vector2(0f, rigidbody2D.velocity.y);
 
-        Vector2 velocity = movement * speed;
-
-        velocity.y = rigidbody2D.velocity.y;
-        
-        if (velocity.x < -maxSpeed)
+        if (0.1 < horizontal || horizontal < -0.1)
         {
-            velocity.x = -maxSpeed;
-        }
-        
-        if (velocity.x > maxSpeed)
-        {
-            velocity.x = maxSpeed;
+            runningTime += Time.deltaTime;
+            stoppedTime = 0;
+            velocity.x = Math.Clamp(runningTime / accelTime * maxSpeed, 0f, maxSpeed) * horizontal;
         }
 
-        if (-0.2 <= horizontal && horizontal <= 0.2)
-        {
-            velocity.x /= stoppingForce;
-        }
-        
         rigidbody2D.velocity = velocity;
     }
 }
