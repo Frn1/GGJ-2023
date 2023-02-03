f9c1845717fb9d0b58dcfbbde6b506446f43086f
frn1 <20214662+frn1@users.noreply.github.com>
2023-02-03 16:03:58 -0300

    Added Piña Spawn

--- Assets/Scripts/SpawnPinia.cs
+++ Assets/Scripts/SpawnPinia.cs
@@ -7,15 +7,21 @@ public class SpawnPinia : MonoBehaviour
     public GameObject pinia;
     public float timer;
     private float countdown;
+
+    private SpriteRenderer _spriteRenderer;
+    
     // Start is called before the first frame update
     void Start()
     {
         countdown = timer;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _spriteRenderer.enabled = false;
         countdown-=Time.deltaTime;  
         if (countdown<=0)
         {
