7ae5de717f7a7637173596f994efe6bdfa441e86
frn1 <20214662+frn1@users.noreply.github.com>
2023-02-05 19:35:42 -0300

    Add branch sounds

--- Assets/Scripts/LeafFall.cs
+++ Assets/Scripts/LeafFall.cs
@@ -74,8 +74,10 @@ public class LeafFall : MonoBehaviour
     private void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
-        countdownFall -= Time.deltaTime;
-        fadeout.a =countdownFall/fallTimer;
+        {
+            countdownFall -= Time.deltaTime;
+            fadeout.a = countdownFall / fallTimer;
+        }
 
     }
     
